d1de4ebfb5e4b0a2cce114c2075f3366b6646351
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2020-12-21 10:21:54 +0100

    Created content of class

--- src/main/java/org/orekit/attitudes/PythonBoundedAttitudeProvider.java
+++ src/main/java/org/orekit/attitudes/PythonBoundedAttitudeProvider.java
@@ -1,3 +1,23 @@
+/* Copyright 2002-2020 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// this file was created by SSC 2020 and is largely a derived work from the
+// original java class/interface
+
 package org.orekit.attitudes;
 
 import org.hipparchus.RealFieldElement;
@@ -7,9 +27,34 @@ import org.orekit.time.FieldAbsoluteDate;
 import org.orekit.utils.FieldPVCoordinatesProvider;
 import org.orekit.utils.PVCoordinatesProvider;
 
-/* TODO: PHY IMPLEMENT */
-
 public class PythonBoundedAttitudeProvider implements BoundedAttitudeProvider {
+
+    /** Part of JCC Python interface to object */
+    private long pythonObject;
+
+    /** Part of JCC Python interface to object */
+    public void pythonExtension(long pythonObject)
+    {
+        this.pythonObject = pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public long pythonExtension()
+    {
+        return this.pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public void finalize()
+            throws Throwable
+    {
+        pythonDecRef();
+    }
+
+    /** Part of JCC Python interface to object */
+    public native void pythonDecRef();
+
+
     /**
      * Compute the attitude corresponding to an orbital state.
      *
@@ -19,9 +64,7 @@ public class PythonBoundedAttitudeProvider implements BoundedAttitudeProvider {
      * @return attitude attitude on the specified date and position-velocity state
      */
     @Override
-    public Attitude getAttitude(PVCoordinatesProvider pvProv, AbsoluteDate date, Frame frame) {
-        return null;
-    }
+    public native Attitude getAttitude(PVCoordinatesProvider pvProv, AbsoluteDate date, Frame frame);
 
     /**
      * Compute the attitude corresponding to an orbital state.
@@ -34,8 +77,19 @@ public class PythonBoundedAttitudeProvider implements BoundedAttitudeProvider {
      */
     @Override
     public <T extends RealFieldElement<T>> FieldAttitude<T> getAttitude(FieldPVCoordinatesProvider<T> pvProv, FieldAbsoluteDate<T> date, Frame frame) {
-        return null;
+        return this.getAttitude_FFF(pvProv, date, frame);
     }
+
+    /**
+     * Compute the attitude corresponding to an orbital state.
+     *
+     * @param pvProv local position-velocity provider around current date
+     * @param date   current date
+     * @param frame  reference frame from which attitude is computed
+     * @return attitude attitude on the specified date and position-velocity state
+     * @since 9.0
+     */
+    public native <T extends RealFieldElement<T>> FieldAttitude<T> getAttitude_FFF(FieldPVCoordinatesProvider<T> pvProv, FieldAbsoluteDate<T> date, Frame frame);
 
     /**
      * Get the first date of the range.
@@ -43,9 +97,7 @@ public class PythonBoundedAttitudeProvider implements BoundedAttitudeProvider {
      * @return the first date of the range
      */
     @Override
-    public AbsoluteDate getMinDate() {
-        return null;
-    }
+    public native AbsoluteDate getMinDate();
 
     /**
      * Get the last date of the range.
@@ -53,7 +105,5 @@ public class PythonBoundedAttitudeProvider implements BoundedAttitudeProvider {
      * @return the last date of the range
      */
     @Override
-    public AbsoluteDate getMaxDate() {
-        return null;
-    }
+    public native AbsoluteDate getMaxDate();
 }
