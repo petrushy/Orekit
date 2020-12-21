9da6488ef6ba5850c8d612e992f26df11d071f4a
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2020-12-21 16:39:13 +0100

    Adding wrapper bodies to classes

--- src/main/java/org/orekit/files/general/PythonAttitudeEphemerisFileWriter.java
+++ src/main/java/org/orekit/files/general/PythonAttitudeEphemerisFileWriter.java
@@ -1,8 +1,54 @@
+/* Copyright 2016 Applied Defense Solutions (ADS)
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * ADS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// this file was created by SSC 2020 and is largely a derived work from the
+// original java class/interface
+
 package org.orekit.files.general;
 
 import java.io.IOException;
 
 public class PythonAttitudeEphemerisFileWriter implements AttitudeEphemerisFileWriter {
+    /** Part of JCC Python interface to object */
+    private long pythonObject;
+
+    /** Part of JCC Python interface to object */
+    public void pythonExtension(long pythonObject)
+    {
+        this.pythonObject = pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public long pythonExtension()
+    {
+        return this.pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public void finalize()
+            throws Throwable
+    {
+        pythonDecRef();
+    }
+
+    /** Part of JCC Python interface to object */
+    public native void pythonDecRef();
+
+
     /**
      * Write the passed in {@link AttitudeEphemerisFile} using the passed in
      * {@link Appendable}.
@@ -15,8 +61,5 @@ public class PythonAttitudeEphemerisFileWriter implements AttitudeEphemerisFileW
      *                     the origin at an unspecified celestial body, etc.)
      */
     @Override
-    public void write(Appendable writer, AttitudeEphemerisFile ephemerisFile) throws IOException {
-/* TODO: Implement this */
-
-    }
+    public native void write(Appendable writer, AttitudeEphemerisFile ephemerisFile) throws IOException;
 }
--- src/main/java/org/orekit/forces/empirical/PythonAccelerationModel.java
+++ src/main/java/org/orekit/forces/empirical/PythonAccelerationModel.java
@@ -1,12 +1,57 @@
+/* Copyright 2002-2020 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// this file was created by SSC 2020 and is largely a derived work from the
+// original java class/interface
+
 package org.orekit.forces.empirical;
 
 import org.hipparchus.RealFieldElement;
 import org.orekit.propagation.FieldSpacecraftState;
 import org.orekit.propagation.SpacecraftState;
 import org.orekit.utils.ParameterDriver;
- /* TODO PHY IMPLEMENT */
 
 public class PythonAccelerationModel implements AccelerationModel {
+
+    /** Part of JCC Python interface to object */
+    private long pythonObject;
+
+    /** Part of JCC Python interface to object */
+    public void pythonExtension(long pythonObject)
+    {
+        this.pythonObject = pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public long pythonExtension()
+    {
+        return this.pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public void finalize()
+            throws Throwable
+    {
+        pythonDecRef();
+    }
+
+    /** Part of JCC Python interface to object */
+    public native void pythonDecRef();
+
     /**
      * Compute the signed amplitude of the acceleration.
      * <p>
@@ -20,9 +65,7 @@ public class PythonAccelerationModel implements AccelerationModel {
      * @return norm of the acceleration
      */
     @Override
-    public double signedAmplitude(SpacecraftState state, double[] parameters) {
-        return 0;
-    }
+    public native double signedAmplitude(SpacecraftState state, double[] parameters);
 
     /**
      * Compute the signed amplitude of the acceleration.
@@ -38,8 +81,10 @@ public class PythonAccelerationModel implements AccelerationModel {
      */
     @Override
     public <T extends RealFieldElement<T>> T signedAmplitude(FieldSpacecraftState<T> state, T[] parameters) {
-        return null;
+        return this.signedAmplitude_FT(state, parameters);
     }
+
+    public native <T extends RealFieldElement<T>> T signedAmplitude_FT(FieldSpacecraftState<T> state, T[] parameters);
 
     /**
      * Get the drivers for acceleration model parameters.
@@ -47,7 +92,5 @@ public class PythonAccelerationModel implements AccelerationModel {
      * @return drivers for acceleration model parameters
      */
     @Override
-    public ParameterDriver[] getParametersDrivers() {
-        return new ParameterDriver[0];
-    }
+    public native ParameterDriver[] getParametersDrivers();
 }
