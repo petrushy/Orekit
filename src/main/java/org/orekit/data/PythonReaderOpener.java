e6ed7846fcb9b24ba13f57581b6a145634aad87b
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2022-10-03 22:06:16 +0200

    Adding PythonReaderOpener and PythonStreamOpener

---
+++ src/main/java/org/orekit/data/PythonReaderOpener.java
@@ -0,0 +1,49 @@
+package org.orekit.data;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class PythonReaderOpener implements DataSource.ReaderOpener {
+
+    /** Part of JCC Python interface to object */
+    private long pythonObject;
+
+    /** Part of JCC Python interface to object */
+    public void pythonExtension(long pythonObject)
+    {
+        this.pythonObject = pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public long pythonExtension()
+    {
+        return this.pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public void finalize()
+            throws Throwable
+    {
+        pythonDecRef();
+    }
+
+    /** Part of JCC Python interface to object */
+    public native void pythonDecRef();
+
+
+    /**
+     * Open the stream once.
+     * <p>
+     * Beware that this interface is only intended for <em>lazy</em> opening a
+     * stream, i.e. to delay this opening (or not open the stream at all).
+     * It is <em>not</em> intended to open the stream several times. Some
+     * implementations may fail if an attempt to open a stream several
+     * times is made. This is particularly true for network-based streams.
+     * </p>
+     *
+     * @return opened stream
+     * @throws IOException if stream cannot be opened
+     */
+    @Override
+    public native Reader openOnce() throws IOException;
+}
