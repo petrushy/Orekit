521111cb7652c700c037a70971abcfd3b3748294
Petrus Hyvönen <petrus.hyvonen@gmail.com>
2022-10-04 07:39:56 +0200

    Added missing PythonEvaluationLogger

---
+++ src/test/java/org/orekit/estimation/common/PythonEvaluationLogger.java
@@ -0,0 +1,59 @@
+/* Copyright 2002-2022 CS GROUP
+ * Licensed to CS GROUP (CS) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * CS licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// this file was created by SSC 2022 and is largely a derived work from the
+// original java class/interface
+
+package org.orekit.estimation.common;
+
+import org.orekit.estimation.measurements.EstimatedMeasurement;
+import org.orekit.estimation.measurements.ObservedMeasurement;
+
+public class PythonEvaluationLogger<T extends ObservedMeasurement<T>> implements EvaluationLogger<T> {
+    /** Part of JCC Python interface to object */
+    private long pythonObject;
+
+    /** Part of JCC Python interface to object */
+    public void pythonExtension(long pythonObject)
+    {
+        this.pythonObject = pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public long pythonExtension()
+    {
+        return this.pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public void finalize()
+            throws Throwable
+    {
+        pythonDecRef();
+    }
+
+    /** Part of JCC Python interface to object */
+    public native void pythonDecRef();
+
+    /**
+     * Log a measurement evaluation.
+     *
+     * @param evaluation measurement evaluation to log
+     */
+    @Override
+    public native void log(EstimatedMeasurement<T> evaluation);
+}
