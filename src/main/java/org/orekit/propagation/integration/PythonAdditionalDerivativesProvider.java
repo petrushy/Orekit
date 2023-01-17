6ef2de9fb232ce794611ecd7e78d503c492871b8
Petrus Hyvönen <petrus.hyvonen@sscspace.com>
2023-01-17 18:54:19 +0100

    Updated AdditionalDerivatesProvider with both Field and standard, exposing the new FieldCombinedDerivatives also.

--- src/main/java/org/orekit/propagation/integration/PythonAdditionalDerivativesProvider.java
+++ src/main/java/org/orekit/propagation/integration/PythonAdditionalDerivativesProvider.java
@@ -1,31 +1,9 @@
-/* Copyright 2002-2022 CS GROUP
- * Licensed to CS GROUP (CS) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * CS licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// this file was created by SSC 2022 and is largely a derived work from the
-// original java class/interface
-
-
 package org.orekit.propagation.integration;
 
-import org.hipparchus.CalculusFieldElement;
-import org.orekit.propagation.FieldSpacecraftState;
-import org.orekit.time.FieldAbsoluteDate;
+import org.orekit.propagation.SpacecraftState;
+import org.orekit.time.AbsoluteDate;
 
-public class PythonFieldAdditionalDerivativesProvider<T extends CalculusFieldElement<T>> implements FieldAdditionalDerivativesProvider<T> {
+public class PythonAdditionalDerivativesProvider implements AdditionalDerivativesProvider {
     /** Part of JCC Python interface to object */
     private long pythonObject;
 
@@ -76,25 +54,25 @@ public class PythonFieldAdditionalDerivativesProvider<T extends CalculusFieldEle
      * @param target       date of propagation
      */
     @Override
-    public native void init(FieldSpacecraftState<T> initialState, FieldAbsoluteDate<T> target);
+    public native void init(SpacecraftState initialState, AbsoluteDate target);
 
     /**
      * Check if this provider should yield so another provider has an opportunity to add missing parts.
      * <p>
-     * Decision to yield is often based on an additional state being {@link FieldSpacecraftState#hasAdditionalState(String)
+     * Decision to yield is often based on an additional state being {@link SpacecraftState#hasAdditionalState(String)
      * already available} in the provided {@code state} (but it could theoretically also depend on
-     * an additional state derivative being {@link FieldSpacecraftState#hasAdditionalStateDerivative(String)
+     * an additional state derivative being {@link SpacecraftState#hasAdditionalStateDerivative(String)
      * already available}, or any other criterion). If for example a provider needs the state transition
      * matrix, it could implement this method as:
      * </p>
      * <pre>{@code
-     * public boolean yield(final FieldSpacecraftState<T> state) {
+     * public boolean yield(final SpacecraftState state) {
      *     return !state.getAdditionalStates().containsKey("STM");
      * }
      * }</pre>
      * <p>
      * The default implementation returns {@code false}, meaning that derivative data can be
-     * {@link #derivatives(FieldSpacecraftState) computed} immediately.
+     * {@link #combinedDerivatives(SpacecraftState) computed} immediately.
      * </p>
      *
      * @param state state to handle
@@ -102,35 +80,36 @@ public class PythonFieldAdditionalDerivativesProvider<T extends CalculusFieldEle
      * as the state is incrementally built up
      */
     @Override
-    public native boolean yield(FieldSpacecraftState<T> state);
+    public native boolean yield(SpacecraftState state);
 
     /**
      * Compute the derivatives related to the additional state parameters.
      *
      * @param s current state information: date, kinematics, attitude, and
      *          additional states this equations depend on (according to the
-     *          {@link #yield(FieldSpacecraftState) yield} method)
+     *          {@link #yield(SpacecraftState) yield} method)
      * @return computed derivatives
+     * @deprecated as of 11.2, replaced by {@link #combinedDerivatives(SpacecraftState)}
      */
 
     @Override
-    public native T[] derivatives(FieldSpacecraftState<T> s);
+    public native double[] derivatives(SpacecraftState s);
 
     /**
      * Compute the derivatives related to the additional state (and optionally main state increments).
      * <p>
      * As of 11.2, there is a default implementation that calls the deprecated
-     * {@link #derivatives(FieldSpacecraftState)} method. This has been done for
+     * {@link #derivatives(SpacecraftState)} method. This has been done for
      * backward compatibility only and will be removed in 12.0.
      * </p>
      *
      * @param s current state information: date, kinematics, attitude, and
      *          additional states this equations depend on (according to the
-     *          {@link #yield(FieldSpacecraftState) yield} method)
+     *          {@link #yield(SpacecraftState) yield} method)
      * @return computed combined derivatives, which may include some incremental
      * coupling effect to add to main state derivatives
      * @since 11.2
      */
     @Override
-    public native FieldCombinedDerivatives<T> combinedDerivatives(FieldSpacecraftState<T> s);
+    public native CombinedDerivatives combinedDerivatives(SpacecraftState s);
 }
