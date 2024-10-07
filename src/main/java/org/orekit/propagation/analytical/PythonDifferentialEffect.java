26033189ae2d570dcd9903b4202b9eee846db7d4
Petrus Hyvönen <petrus.hyvonen@sscspace.com>
2024-10-07 07:10:59 +0200

    Adding PythonDifferentialEffect extension for AdapterPropagator.DifferentialEffect

---
+++ src/main/java/org/orekit/propagation/analytical/PythonDifferentialEffect.java
@@ -0,0 +1,34 @@
+package org.orekit.propagation.analytical;
+
+import org.orekit.propagation.SpacecraftState;
+
+public class PythonDifferentialEffect implements AdapterPropagator.DifferentialEffect {
+    /** Part of JCC Python interface to object */
+    private long pythonObject;
+
+    /** Part of JCC Python interface to object */
+    public void pythonExtension(long pythonObject)
+    {
+        this.pythonObject = pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public long pythonExtension()
+    {
+        return this.pythonObject;
+    }
+
+    /** Part of JCC Python interface to object */
+    public void finalize()
+            throws Throwable
+    {
+        pythonDecRef();
+    }
+
+    /** Part of JCC Python interface to object */
+    public native void pythonDecRef();
+
+
+    @Override
+    public native SpacecraftState apply(SpacecraftState original);
+}
